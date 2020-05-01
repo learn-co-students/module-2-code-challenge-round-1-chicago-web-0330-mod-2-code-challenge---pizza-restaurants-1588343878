ed4086bcc658bde30e7fb8f871665ca6cf81813e
github username <github email address>
2020-05-01 11:22:54 -0500

    done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,13 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def show
+    @restaurants = Restaurant.find(params[:id])
+  end
+
 end
