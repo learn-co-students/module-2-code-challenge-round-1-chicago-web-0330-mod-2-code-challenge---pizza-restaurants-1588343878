0ad48cb8af299432b3fa428e2ca39fcca9c206e1
Auliz <joseph.auz77@gmail.com>
2020-05-01 11:22:06 -0500

    Joe's commit for Mod 2 code challenge

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,2 +1,7 @@
 class Pizza < ApplicationRecord
+  has_many :restaurant_pizza
+  has_many :restaurants, through: :restaurant_pizza
+
+  validates_presence_of :name, :ingredients 
+  validates_uniqueness_of :name
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,8 @@
 class Restaurant < ApplicationRecord
-  
+  has_many :restaurant_pizza
+  has_many :pizzas, through: :restaurant_pizza
+
+
+
+
 end
