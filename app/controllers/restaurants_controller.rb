7eecc7485b6677dc4a3cff6dbf48b496d4c76fb7
aseleidy <ash.etemadi@gmail.com>
2020-05-01 12:22:19 -0400

    Module 2 Code Challenge

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,31 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
+  def show 
+    @restaurant = Restaurant.find(params[:id])
+  end 
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end 
+
+  def update 
+    @restaurant = Restaurant.find(params[:id])
+    params[:restaurant][:pizzas].shift
+
+    params[:restaurant][:pizzas].each do |pizza_id|
+      @restaurant.pizzas << Pizza.find(pizza_id)
+    end 
+
+    if @restaurant.update(rest_params)
+      redirect_to @restaurant
+    else 
+      render 'edit'
+    end 
+  end
+  
+  def rest_params
+    params.require(:restaurant).permit(:name, :address)
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :pizzas 
+  resources :restaurants
+  
   get '/restaurants', to: 'restaurants#index'
   get '/pizzas', to: 'pizzas#index'
+  
 end
