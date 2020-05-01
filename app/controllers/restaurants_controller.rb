8ac17e4f9bc2ea52600ee3557ac2e8000f18d92e
Shau Yin Luk <ivan.luk@icloud.com>
2020-05-01 11:22:22 -0500

    Ivan Luk code challenge

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,31 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+    @pizzas = Pizza.all
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    
+      if @restaurant.update(restaurant_params)
+        
+        redirect_to @restaurant
+      else 
+      
+        render :edit
+      end
+  end
+  private
+
+  def restaurant_params
+      params.require(:restaurant).permit(:name, :address, pizza_ids:[])
+  end
+
 end
