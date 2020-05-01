42a358fb90e19dfff8ea42b0d3bc8948d75899d6
jayastronomic <juliansmith94@hotmail.com>
2020-05-01 11:27:27 -0500

    I got a lot to catch up on

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -2,4 +2,18 @@ class PizzasController < ApplicationController
   def index
     @pizzas = Pizza.all
   end
+
+  def show
+    @pizza = Pizza.find(params[:id])
+  end
+
+  def new
+    @pizza = Pizza.new
+  end
+
+  def create
+    @pizza = Pizza.create(name: params[:pizza][:name], ingredients: params[:pizza][:ingredients])
+    redirect_to pizza_path(@pizza)
+  end
+
 end
