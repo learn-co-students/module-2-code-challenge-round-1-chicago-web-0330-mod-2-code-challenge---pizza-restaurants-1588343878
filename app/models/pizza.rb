dcd418dc09924ee26b017fade528f32c83140bb0
owenabbott <ocabbott@gmail.com>
2020-05-01 11:22:33 -0500

    mod2challenge

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,2 +1,13 @@
 class Pizza < ApplicationRecord
+    validates_presence_of :name, :ingredients
+    has_many :restaurant_pizzas
+    has_many :restaurants, through: :restaurant_pizzas
 end
+
+
+# Add validations to the Pizza model so that each pizza must have:
+
+# - a name
+# - ingredients
+
+# Add error handling to the Pizza create action. Show any validation errors on the Pizza creation form if the user attempts to save an invalid Pizza.
